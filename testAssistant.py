cbd3dd15d015fd884e9971518371ec7d0287e510
declankra <declankramper@gmail.com>
2024-04-02 11:20:53 -0500

    add thread, run, meeting templates

--- testAssistant.py
+++ testAssistant.py
@@ -9,4 +9,35 @@ client = OpenAI(
     api_key=OPENAI_KEY,
 )
 ASSISTANT_KEY = os.getenv("ASSISTANT_KEY")
-my_assistant = client.beta.assistants.retrieve(ASSISTANT_KEY)
+meet_or_not_asst = client.beta.assistants.retrieve(ASSISTANT_KEY)
+
+## import user inputs for meeting info && parse into message/file
+
+## create thread
+new_thread = client.beta.threads.create(
+  messages=[
+    {
+      "role": "user",
+      "content": "Crate a meeting agenda based off the following user's purpose, expected outcome type, expected outcome, and priority.",
+      ##"file_ids": [file_id]
+    }
+  ]
+)
+
+thread_id = new_thread.id
+
+run = client.beta.threads.runs.create(
+  thread_id=thread_id,
+  assistant_id=meet_or_not_asst
+)
+run_id = run.id
+
+run = client.beta.threads.runs.retrieve(
+  thread_id=thread_id,
+  run_id=run_id
+)
+
+thread_messages = client.beta.threads.messages.list(thread_id)
+print(thread_messages.data)
+
+
